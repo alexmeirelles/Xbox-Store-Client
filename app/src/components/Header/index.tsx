b437e31ae5bdafe22fd032cae3610d954d6465df
alexmeirelles <alemeirelles@msn.com>
2022-07-07 10:09:00 -0300

    conflicts package

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -1,6 +1,19 @@
+import logo from '../../assets/images/xbox.png'
+import './style.css'
+
 const Header = () => {
   return (
-    <div>index</div>
+    <section className='head'>
+      <div className='left'>
+        <div>profile</div>
+      </div>
+        <div className='right'>      
+          <div>
+            <img src={logo} alt="logo xbox" className='header-logo'/>
+          </div>
+          <div>data</div>
+        </div>
+    </section>
   )
 }
 
--- app/src/components/card.style.ts
+++ app/src/components/card.style.ts
@@ -3,13 +3,13 @@ import styled from "styled-components";
 export const CardStyle = styled.section`
   display: flex;
   width: 30%;
-  height: 20%;
+  height: 50%;
   align-items: center;
   padding: 15px;
   justify-content: center;
   flex-direction: column;
-  background-color: gray;
   margin: 10px;
-  border-radius: 15px;
-  color: #00ff00;
+  border-radius: 41px;
+  background: #e0e0e0;
+  box-shadow: 14px 14px 25px #bebebe, -14px -14px 25px #ffffff;
 `;
